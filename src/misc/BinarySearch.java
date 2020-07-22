436edaa8892a8613766874ef66a9ec921a0d4e8a
Robin Gautam <r.gautam@cvent.com>
2020-07-23 00:32:34 +0530

    feat: Binary Search and Array Rotation

---
+++ src/misc/BinarySearch.java
@@ -0,0 +1,68 @@
+package misc;
+
+import algorithm.Algorithm;
+import algorithm.BasicUtilityProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class BinarySearch implements Algorithm {
+    private BasicUtilityProvider.Utilities utilities;
+
+    public BinarySearch() {
+        utilities = BasicUtilityProvider.getUtilities();
+    }
+
+    @Override
+    public String getName() {
+        return "Binary Search";
+    }
+
+    private boolean hasElement(List<Integer> numbers, int searchKey) {
+        return performBinarySearch(numbers, searchKey, 0, numbers.size() - 1);
+    }
+
+    private boolean performBinarySearch(List<Integer> numbers, int key, int lowerBound, int upperBound) {
+        if (lowerBound > upperBound) {
+            return false;
+        }
+
+        // Check the middle element
+        int middleIndex = (lowerBound + upperBound) / 2;
+        if (numbers.get(middleIndex) == key) {
+            return true;
+        }
+
+        // Figure out which sub-array we need to search in
+        if (key < numbers.get(middleIndex)) {
+            return performBinarySearch(numbers, key, lowerBound, middleIndex - 1);
+        } else {
+            return performBinarySearch(numbers, key, middleIndex + 1, upperBound);
+        }
+    }
+
+    @Override
+    public boolean test() {
+        List<Integer> sortedEvenNumbers = new ArrayList<>();
+        int counter = 0;
+        while (counter < 1000) {
+            sortedEvenNumbers.add(counter++ * 2);
+        }
+
+        List<Supplier<Boolean>> tests = new ArrayList<>();
+        // Edge cases
+        tests.add(() -> hasElement(sortedEvenNumbers, 0));
+        tests.add(() -> hasElement(sortedEvenNumbers, 998));
+
+        // Not found cases
+        tests.add(() -> !hasElement(sortedEvenNumbers, -1));
+        tests.add(() -> !hasElement(sortedEvenNumbers, 1023));
+
+        // Must be found, random cases
+        tests.add(() -> hasElement(sortedEvenNumbers, utilities.getRandomInt(400) * 2));
+        tests.add(() -> hasElement(sortedEvenNumbers, utilities.getRandomInt(400) * 2));
+
+        return runAssertions(tests);
+    }
+}
--- src/misc/RotateArray.java
+++ src/misc/RotateArray.java
@@ -3,6 +3,7 @@ package misc;
 import algorithm.Algorithm;
 import algorithm.BasicUtilityProvider;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RotateArray implements Algorithm {
@@ -12,32 +13,70 @@ public class RotateArray implements Algorithm {
         utilities = BasicUtilityProvider.getUtilities();
     }
 
-    private void rotate(List<Integer> array, int rotateByNumberOfElements) {
+    private List<Integer> rotate(List<Integer> arrays, int rotateByNumberOfElements) {
+        List<Integer> array = new ArrayList<>(arrays);
+        rotateByNumberOfElements = rotateByNumberOfElements % array.size();
         int noOfTimesToPerformRotation = GCD.findGCD(array.size(), rotateByNumberOfElements);
         for (int i = 0; i < noOfTimesToPerformRotation; i++) {
             // from Index i, rotate till it reaches back to i
             // 1 2 3 4 5 6 7 8
             performRotation(i, rotateByNumberOfElements, array);
         }
+        return array;
     }
 
     private void performRotation(int startingIndex, int rotateByNumberOfElements, List<Integer> array) {
+        int arraySize = array.size();
         int i = startingIndex;
-        int j = (startingIndex - rotateByNumberOfElements) > 0
-                ? (startingIndex - rotateByNumberOfElements)
-                : array.size() + (startingIndex - rotateByNumberOfElements);
+        int j = getPreviousIndex(i, rotateByNumberOfElements, arraySize);
         while (j != startingIndex) {
             utilities.swapItem(array, i, j);
+            int olderJ = j;
+            j = getPreviousIndex(j, rotateByNumberOfElements, arraySize);
+            i = olderJ;
+        }
+    }
 
-            // Todo: Finish this
-            int temp = j;
-            j = j - rotateByNumberOfElements;
-            i = j;
+    private int getPreviousIndex(int from, int steps, int arraySize) {
+        if (from - steps < 0) {
+            return (arraySize + from - steps);
+        } else {
+            return from - steps;
         }
     }
 
     @Override
     public boolean test() {
-        return false;
+        List<Integer> testList = getRandomList();
+        int timesToBeRotated = utilities.getRandomInt(1000);
+        List<Integer> rotatedList1 = getRotatedListUsingInefficientMethod(testList, timesToBeRotated);
+        List<Integer> rotatedList2 = rotate(testList, timesToBeRotated);
+        for (int i = 0; i < testList.size(); i++) {
+            if (!rotatedList1.get(i).equals(rotatedList2.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<Integer> getRotatedListUsingInefficientMethod(List<Integer> list, int times) {
+        int size = list.size();
+        times = times % size;
+        int j = times;
+        List<Integer> rotatedArray = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            j = j % size;
+            rotatedArray.add(list.get(j));
+            j++;
+        }
+        return rotatedArray;
+    }
+
+    private List<Integer> getRandomList() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < utilities.getRandomInt(100); i++) {
+            list.add(i);
+        }
+        return list;
     }
 }
